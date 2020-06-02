6415ffaff6a62c925693d1a7a87446e429f4f4b1
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-06-02 18:35:50 +0700

    change favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#3895D3`,
         theme_color: `#3895D3`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/vyonizr-web-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
