774dc209405fcabc5051880722d75ad834f7e2a2
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-06-02 18:37:02 +0700

    edit custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,21 @@
 import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components";
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const WhiteLink = styled(Link)`
+  color: white;
+`
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="404" />
+    <div style={{margin: 'auto'}}>
+      <h1>page not found</h1>
+      <WhiteLink to="/">return to homepage</WhiteLink>
+    </div>
   </Layout>
 )
 
