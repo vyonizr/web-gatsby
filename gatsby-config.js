4fd90c77ddac3b8342a20cfaad9ff9e04f95a3a0
Fitrahtur Rahman <fitrahtur@gmail.com>
2020-06-08 15:17:47 +0700

    add source file for fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        name: `fonts`,
+        path: `${__dirname}/src/fonts`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
